d99138f74e581f3c7fd84086146c00c59ff1f0d4
jeremyrauber <jeremy_rauber@live.com>
2017-09-20 12:00:15 -0300

    Update db.sql

--- Integrador-2/WebContent/db/db.sql
+++ Integrador-2/WebContent/db/db.sql
@@ -97,7 +97,7 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `projeto`.`usuario_has_atividade` (
   `usuario_id` INT NOT NULL,
   `atividade_id_atividade` INT NOT NULL,
-  `data_fim_atividade` TIMESTAMP NOT NULL,
+  `data_fim_atividade` DATETIME DEFAULT CURRENT_TIMESTAMP,
   `status` TINYINT(1) NULL,
   `caminho_imagem` VARCHAR(255) NULL,
   PRIMARY KEY (`usuario_id`, `atividade_id_atividade`),
@@ -129,11 +129,11 @@ ENGINE = InnoDB;
 ALTER TABLE mestre ADD UNIQUE (login);
 ALTER TABLE evento ADD COLUMN  palavra_chave varchar(255);
 
-INSERT INTO mestre (`id_mestre`, `nome`, `data_nasc`, `login`, `senha`, `endereco`, `email`, `ativo`, `cep`, `bairro`, `cidade`, `estado`,`senhanu`) VALUES (1, 'Jeremy', '1990-01-21', 'admin', '21232f297a57a5a743894a0e4a801fc3', 'rua puma, n 224', 'jeremy_rauber@live.com',1, '85860-240', 'Vila A','Foz do Iguaçu', 'PR','admin');
-INSERT INTO evento (data_fim, data_inicio, descricao, mestre_id_mestre, nome, palavra_chave, id) values     ('2017-09-14', '2017-09-16', ' Cacar mosquitos valentoes no bairro', 1, 'Aedes na mira', 'adeus egypt', 2);
-INSERT INTO usuario (`id`,`nome`,`login`,`senha`,`endereco`,`data_nasc`,`bairro`, `cidade`,`cep`,`email`,`ativo`,`estado`,`banido`,`data_cadastro`) VALUES (1, 'Jobelino das Coves', 'jobe',  'eb62f6b9306db575c2d596b1279627a4', 'av morenitas n 225','2005-10-12','Vilac','Foz do Iguaçu', '85550-040', 'jobedascove@email.com',1,'PR',1,'2017-09-14');
+INSERT INTO mestre (`id_mestre`, `nome`, `data_nasc`, `login`, `senha`, `endereco`, `email`, `ativo`, `cep`, `bairro`, `cidade`, `estado`) VALUES (1, 'Jeremy', '1990-01-21', 'admin', '21232f297a57a5a743894a0e4a801fc3', 'rua puma, n 224', 'jeremy_rauber@live.com',1, '85860-240', 'Vila A','Foz do IguaÃ§u', 'PR');
+INSERT INTO evento (data_fim, data_inicio, descricao, mestre_id_mestre, nome, palavra_chave, id) values     ('2017-09-14', '2017-09-16', ' Cacar mosquitos valentoes no bairro', 1, 'Aedes na mira', 'adeus egypt', 1);
+INSERT INTO usuario (`id`,`nome`,`login`,`senha`,`endereco`,`data_nasc`,`bairro`, `cidade`,`cep`,`email`,`ativo`,`estado`,`banido`,`data_cadastro`) VALUES (1, 'Jobelino das Coves', 'jobe',  'eb62f6b9306db575c2d596b1279627a4', 'av morenitas n 225','2005-10-12','Vilac','Foz do IguaÃ§u', '85550-040', 'jobedascove@email.com',1,'PR',1,'2017-09-14');
 INSERT INTO atividade(id_atividade,descricao,nivel) VALUES (1,"recolher pneus do quintal",3);
 INSERT INTO atividade(id_atividade,descricao,nivel) VALUES (2,"Limpar vasos de plantas",1);
 INSERT INTO evento_has_atividade(evento_id,evento_mestre_id_mestre,atividade_id_atividade) VALUES (1,1,1);
-INSERT INTO usuario_has_atividade
-insert into evento_has_usuario (evento_id,evento_mestre_id_mestre,usuario_id,banido_evento) values (1,1,1,0);
+INSERT INTO usuario_has_atividade(`usuario_id`,`atividade_id_atividade`,`data_fim_atividade`,`status`,`caminho_imagem`) VALUES (1,1,'2014-10-01',0,'C:\\tempp\\usuario_1_envio_1.jpg');
+INSERT INTO evento_has_usuario (evento_id,evento_mestre_id_mestre,usuario_id,banido_evento) VALUES (1,1,1,0);
